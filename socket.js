2dd52ce2a1889a72438b219cc717d16d4eb8a6d9
Isaac Ndala <isaacndala111@gmail.com>
2022-05-15 17:19:35 +0100

    Add origin to socket io initialization

--- socket.js
+++ socket.js
@@ -4,7 +4,7 @@ module.exports = {
     init: httpServer => {
         io = require('socket.io')(httpServer, {
             cors: {
-              origin: "http://localhost:8080",
+              origin: "https://wherechat.herokuapp.com/",
               methods: ["GET", "POST"]
             }
         })
